44f5fef80ff31a1f0ab22959fdcdca1ae7fa6033
Deepak Kumar <deepakdk2431@gmail.com>
2019-10-02 01:22:02 +0530

    added django-heroku

--- djangoBlog/settings.py
+++ djangoBlog/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -133,3 +134,5 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 LOGIN_REDIRECT_URL = 'blog-home'
 
 LOGIN_URL = 'login'
+
+django_heroku.settings(locals())
