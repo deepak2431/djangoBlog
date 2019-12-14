7ae5b98f1cd386e7f18edd0e07993e5566be5c01
moraxella <ankitsainiz865@gmail.com>
2019-12-14 15:09:51 +0530

    Recover password

--- djangoBlog/urls.py
+++ djangoBlog/urls.py
@@ -26,7 +26,8 @@ urlpatterns = [
     path('register/', user_views.register, name="register"),
     path('profile/', user_views.profile, name="profile"),
     path('login/', auth_views.LoginView.as_view(template_name='user/login.html'), name="login"),
-    path('logout/', auth_views.LogoutView.as_view(template_name='user/logout.html'), name="logout"), 
+    path('logout/', auth_views.LogoutView.as_view(template_name='user/logout.html'), name="logout"),
+    path('recover/', auth_views.LogoutView.as_view(template_name='user/recover.html'), name="recover"),
     path('', include('blog.urls')),
 
 ]
