f74bffcd35928d40c05548df5f76cfde41f14699
Administrator <Administrator@TeamCity>
2020-12-24 10:12:51 +0000

    TeamCity change in 'DjangoBlog' project: runners of 'Build and Push' build configuration were updated

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -64,7 +64,7 @@ object Build : BuildType({
         dockerCommand {
             name = "Push blog"
             commandType = push {
-                namesAndTags = "revolyram/djangoblog"
+                namesAndTags = "revolyram/djangoblog:%teamcity.build.branch%"
             }
         }
     }
