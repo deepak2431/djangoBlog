37d632ff0b4fc0b22e2150ef0a7b50f74f451f31
Justin Jefferson <jjefferson@pinnacle21.com>
2023-06-21 13:16:34 -0400

    Task admin inline created

--- recruit/admin.py
+++ recruit/admin.py
@@ -1,7 +1,17 @@
 from django.contrib import admin
 from .models import Student, Event, Task
 
+
+class TaskInline(admin.TabularInline):
+    model = Task
+
+
+class StudentAdmin(admin.ModelAdmin):
+    inlines = [
+        TaskInline
+    ]
+
+
 # Register your models here.
-admin.site.register(Student)
+admin.site.register(Student, StudentAdmin)
 admin.site.register(Event)
-admin.site.register(Task)
--- recruit/views.py
+++ recruit/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
+from datetime import date
 from .forms import CandidateCheckinForm
-from .models import Student
+from .models import Student, Task
 
 
 def checkin(request):
@@ -8,12 +9,34 @@ def checkin(request):
 
     # create object of form
     form = CandidateCheckinForm(request.POST or None, request.FILES or None)
-
+    print(request)
     # check if form data is valid
     if form.is_valid():
+        print("Form Valid")
         # save the form data to model
         student = Student.objects.create(name=request.POST.get("name"), email=request.POST.get("email"))
         student.save()
+
+        tasks = [
+            "Initial Email"
+            "Resume",
+            "Schedule Assessment",
+            "Assessment Completed",
+            "Assessment Feedback",
+            "Schedule Interview",
+            "Interview Completed",
+            "Interview Feedback"
+        ]
+
+        for task in tasks:
+            t = Task.objects.create(
+                name=task,
+                status=Task.INCOMPLETE,
+                student=student,
+                start_date=date.today(),
+                update_date=date.today(),
+            )
+            t.save()
 
     context['form'] = form
     return render(request, "recruit/checkin.html", context)
