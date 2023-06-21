d4e526e1ee300b919ccb1debbb32569b3ed0590b
sue gerace <sgerace@pinnacle21.com>
2023-06-21 15:05:35 -0400

    updating django admin header to say recruiter

--- blog/admin.py
+++ blog/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from .models import Post, Comment, Images
 
+admin.site.site_header = 'Recruiter'
+
 # Register your models here.
 admin.site.register(Post)
 admin.site.register(Comment)
